282c5497256e0d3f4d444e7d3434e60c60a94cc5
yaoki87 <878787yao@gmail.com>
2021-10-25 12:32:34 +0000

    4-10 index前

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
   root to: 'homes#top'
+  get '/home/about' => 'homes#about'
   devise_for :users
-  
-  resources :books, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show]
+
+  resources :books
+  resources :users, only: [:show, :index, :edit, :update]
 end
