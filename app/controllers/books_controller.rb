fff11bfcd07d00124d4582618d14b202b34e6fd9
yaoki87 <878787yao@gmail.com>
2021-10-27 05:13:02 +0000

    4-10index左側

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,34 +1,38 @@
 class BooksController < ApplicationController
-  
+
   def new
+   @book = Book.new
   end
 
   def create
-
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(current_user)
   end
 
   def index
-    
+    @user = current_user
+    @book = Book.new
   end
 
   def show
-    
+   @book = current_user
   end
 
   def destroy
-    
+
   end
 
   def edit
   end
-  
+
   def update
   end
-  
+
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,20 @@
 class UsersController < ApplicationController
   def show
   end
-  
+
   def index
   end
-  
+
   def edit
+    @user = current_user
   end
-  
+
   def update
   end
-  
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
 end
