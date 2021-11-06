77a90284c09a7c645b4568f85d063b9d63b65598
yaoki87 <878787yao@gmail.com>
2021-11-06 16:09:21 +0000

    Updateフォロー機能途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   root to: 'homes#top'
   get '/home/about' => 'homes#about'
   devise_for :users, controllers: {   registrations: 'users/registrations',
@@ -8,6 +10,11 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:show, :index, :edit, :update]
+  
+  resources :users, only: [:show, :index, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 
 end
